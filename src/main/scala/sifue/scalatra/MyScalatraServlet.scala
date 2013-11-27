a2999e1f84cfec50bbe5b9fdf7dc639d27bd88f7
Denis Cornehl <denis.cornehl@gmail.com>
2013-11-27 11:04:47 +0100

    prettier demo-page for cloudControl

--- src/main/scala/sifue/scalatra/MyScalatraServlet.scala
+++ src/main/scala/sifue/scalatra/MyScalatraServlet.scala
@@ -7,10 +7,17 @@ class MyScalatraServlet extends ScalatraServlet with ScalateSupport {
 
   get("/") {
     <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
+    <head>
+        <link href='css/helloworld.css' rel='stylesheet' type='text/css'/>
+        <title>Hello World in Scala</title>
+    </head>
+    <body>
+        <strong>Hello world, I'm a Scala app running on cloudControl!</strong>
+      <footer>
+        Coming from the <a href="https://www.cloudcontrol.com/dev-center/Quickstart">Quickstart</a>?
+        Check out <a href="https://www.cloudcontrol.com/dev-center/Guides/Java/Scala">the explanation</a>.
+      </footer>
+    </body>
     </html>
   }
 
